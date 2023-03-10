900b73fb9f8f72dfa6a8cad3207bfb9838a108ce
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-10 14:14:59 +0100

    INIT With basic assets and JS pseudo-code

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*
+Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz”
+per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+BONUS 1:
+Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+Potete usare vari strumenti per farlo:
+`append()` oppure
+`.innerHTML`
+`.insertAdjacentHTML()`
+A voi la scelta
+
+BONUS 2:
+Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+*/
+
+/* 
+TOOLS
+- let / const
+- for cycle
+- % division reminder
+- console.log
+*/
